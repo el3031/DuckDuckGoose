c9c1504295acf6d9b304bb5c1aa849ba75c03e12
el3031 <67562724+el3031@users.noreply.github.com>
2022-04-12 15:24:25 -0400

    set up action based stuff

---
+++ Assets/Scripts/Pet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pet : MonoBehaviour
+{
+    [SerializeField] private GameObject hearts;
+    [SerializeField] private float offset;
+    [SerializeField]
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnHoverEnter()
+    {
+        Instantiate(hearts, transform.position + Vector3.up * offset);
+    }
+}
