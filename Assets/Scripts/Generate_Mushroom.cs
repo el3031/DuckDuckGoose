aa60754b774c7fb73be20d6946e2483cd5d4d22b
Sandy Zhao <bz2328@columbia.edu>
2022-05-08 18:07:07 -0400

    random mushrooms generated

---
+++ Assets/Scripts/Generate_Mushroom.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate_Mushroom : MonoBehaviour
+{
+    private GameObject[] mushroomObj;
+    private int xSize = 50;
+    private int ySize = 50;
+    private float GridSpaceSize = 1f; //will have to change to include scaling later
+    private GameObject[,] grid;
+    private List<Vector3> mushroom_Dist = new List<Vector3>();
+    private float maxDist; //audio cue distance for mushroom
+
+    [SerializeField] private GameObject mushroomMonsterObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new GameObject[xSize, ySize];
+        maxDist = mushroomMonsterObj.GetComponent<AudioSource>().maxDistance;
+        Debug.Log("mushroom audio distance: " + maxDist);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //var gos = GameObject[];
+        //mushroomObj = GameObject.FindGameObjectsWithTag("Mushroom_Monster");
+        //Debug.Log("Length of mushroomObj:" + mushroomObj.Length);
+        checkMushroomNum();
+    }
+
+    private void checkMushroomNum()
+    {
+        bool isaddMushroom = true;
+        mushroomObj = GameObject.FindGameObjectsWithTag("Mushroom_Monster");
+        if(mushroomObj.Length < 5)
+        {
+            //assign x and y positions
+            int x = Random.Range(1, xSize);
+            int y = Random.Range(1, ySize);
+            Vector3 new_pos = new Vector3(x * GridSpaceSize, 0, y * GridSpaceSize);
+
+            //update mushroom position list
+            if (mushroom_Dist.Count >= 5)
+            {
+                mushroom_Dist.RemoveAt(0);
+            }
+
+            //compare distance
+            foreach (Vector3 pos in mushroom_Dist)
+            {
+                if(Vector3.Distance(pos, new_pos) < maxDist)
+                {
+                    isaddMushroom = false;
+                }
+            }
+
+            //generate mushroom
+            if (isaddMushroom)
+            {
+                grid[x, y] = Instantiate(mushroomMonsterObj, new_pos, Quaternion.identity);
+                grid[x, y].transform.parent = transform;
+                Debug.Log("new mushroom created");
+
+                //add to the mushroom distance list
+                mushroom_Dist.Add(grid[x, y].transform.position);
+                Debug.Log("The list of mushroom positions" + mushroom_Dist.Count);
+            }
+
+            //Debug.Log("Distance to origin: " + Vector3.Distance(grid[x, y].transform.position, origin));
+
+        }
+    }
+}
